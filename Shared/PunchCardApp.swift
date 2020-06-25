601b10cacd135df619c39a4724ee89cb8504ae18
= <cousillas806@gmail.com>
2020-06-25 08:44:12 -0300

    Initial Commit

---
+++ Shared/PunchCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PunchCardApp.swift
+//  Shared
+//
+//  Created by Mauricio Cousillas on 6/25/20.
+//
+
+import SwiftUI
+
+@main
+struct PunchCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
